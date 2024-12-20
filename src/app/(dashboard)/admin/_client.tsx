ed6c731b6c96ff08324bc705f4e6e2b1dfd034e2
Doljinsuren <doljinsuren.t@techpack.mn>
2024-12-20 17:43:59 +0800

    new feature: teacher info card

--- src/app/(dashboard)/admin/_client.tsx
+++ src/app/(dashboard)/admin/_client.tsx
@@ -1,10 +1,27 @@
+import { AdminInfoCards } from "@/components/admin/admin-info-cards"
+// import { RadialBarComponent } from "@/components/admin/count-chart"
 import { Container } from "@/components/base/container"
 
 export const ClientRenderer = () => {
+  const datas = [
+    { name: "Users", count: 100 },
+    { name: "Parent", count: 100 },
+    { name: "Teachers", count: 100 },
+    { name: "Total", count: 100 }
+  ]
   return (
     <Container>
       <div className='w-full flex flex-col md:flex-row'>
-        <div className='w-full lg:w-2/3'> L </div>
+        <div className='w-full lg:w-2/3 flex gap-2 justify-between flex-wrap'>
+          {datas.map(data => (
+            <AdminInfoCards
+              key={data.name}
+              type={data.name}
+              total={data.count}
+            />
+          ))}
+        </div>
+        {/* <RadialBarComponent /> */}
 
         <div className='w-full lg:w-1/3'> R </div>
       </div>
--- src/components/admin/admin-info-cards.tsx
+++ src/components/admin/admin-info-cards.tsx
@@ -1,3 +1,18 @@
-export const AdminInfoCards = () => {
-  return <div>jj</div>
+"use client"
+import { Typography } from "antd"
+const { Text } = Typography
+
+export const AdminInfoCards = ({
+  total,
+  type
+}: {
+  total: number
+  type: string
+}) => {
+  return (
+    <div className='rounded-2xl odd:bg-dashPurple even:bg-dashYellow flex-1 p-4 flex flex-col'>
+      <Text>{type}</Text>
+      <Text>{total}</Text>
+    </div>
+  )
 }
